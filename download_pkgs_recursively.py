a4247a6352849d4ba0fbbbf1af21fd871cadbe9f
user8191 <100133685+user8191@users.noreply.github.com>
2023-10-18 10:22:52 +0300

    Updated

--- download_pkgs_recursively.py
+++ download_pkgs_recursively.py
@@ -1,4 +1,5 @@
 
+import functools
 import yaml
 import io
 import os
@@ -11,24 +12,43 @@ OUTPUT_DIR = r".\pkgs"
 PKG_URL_PREFIX = r"https://pkg.freebsd.org/FreeBSD:13:amd64/latest"
 PKGS_URLS_JSON_PATH = r"C:\users\user\Downloads\packagesite.yaml"
 MANIFEST_FILENAME = "+MANIFEST"
+HTTP_SUCCESS = 200
 
 
 PKGS_TO_DOWNLOAD = [
 # Example: {"name": "abc", "version": "1.2.3"}
 ]
 
-
 class PkgNotFoundException(Exception):
 	pass
 
 
-# Bad implementation, can be improved
-def create_url_with_version(pkg_obj, package_name, package_version):
+class BadUrlException(Exception):
+	pass
+
+
+def log_decorator(func):
+	def wrapper(*args, **kwargs):
+		print("Started running {func_name} with params {args} {kwargs}".format(func_name=func.__name__, args=args, kwargs=kwargs))
+		retval = func(*args, **kwargs)
+		print("Finished running {func_name}, return value: {retval}".format(func_name=func.__name__, retval=retval))
+		return retval
+	return wrapper
+
+
+def should_attempt_download_without_yaml_parse(package_version):
+	return package_version is not None
+
+
+@log_decorator
+def create_url_without_yaml_parse(package_name, package_version):
 	pkg_name_and_version = package_name + '-' + package_version + ".pkg"
 	return r'/'.join([PKG_URL_PREFIX, "All", pkg_name_and_version])
 
 
-def parse_url_from_json(package_name, package_version=None):
+# TODO: Currently doesn't check package version, can be improved
+@log_decorator
+def parse_url_from_yaml(package_name):		
 	with open(PKGS_URLS_JSON_PATH, "r") as fp:
 		fp = io.TextIOWrapper(fp.buffer, errors='replace')
 
@@ -38,23 +58,15 @@ def parse_url_from_json(package_name, package_version=None):
 			pkg_objs = yaml.load_all(line, Loader=yaml.FullLoader)
 			for pkg_obj in pkg_objs:
 				if pkg_obj["name"] == package_name:
-					pkg_url = '/'.join([PKG_URL_PREFIX, pkg_obj["path"]])
-
-					# Implementation can be improved to check if name+version matches url in yaml
-					if package_version is not None:
-						return create_url_with_version(pkg_obj, package_name, package_version)
-					return pkg_url
+					return '/'.join([PKG_URL_PREFIX, pkg_obj["path"]])
 
 				line = fp.readline()
 
-	if package_version is not None:
-		logging.error("Package {package_name}-{package_version} not found".format(package_name=package_name, package_version=package_version))
-	else:
 		logging.error("Package {package_name} not found".format(package_name=package_name))
 
 
-def download_single_package(package_name, package_version=None):
-	url = parse_url_from_json(package_name, package_version)
+@log_decorator
+def attempt_download(url):
 	local_package_path = os.path.join(OUTPUT_DIR, url.split(r'/')[-1])
 
 	if os.path.exists(local_package_path):
@@ -62,14 +74,35 @@ def download_single_package(package_name, package_version=None):
 
 	response = requests.get(url)
 
+	if response.status_code != HTTP_SUCCESS:
+		raise BadUrlException("Status not 200, bad url")
+
 	with open(local_package_path, "wb") as fp:
 		fp.write(response.content)
 
 	return local_package_path
 
 
+def download_single_package(package_name, package_version=None):
+	if should_attempt_download_without_yaml_parse(package_version):
+		try:
+			url = create_url_without_yaml_parse(package_name, package_version)
+			local_package_path = attempt_download(url)
+		except:
+			url = parse_url_from_yaml(package_name, package_version)
+			local_package_path = attempt_download(url)
+	else:
+		url = parse_url_from_yaml(package_name)
+		local_package_path = attempt_download(url)
+
+	return local_package_path
+
+
 def extract_dependencies(local_package_path):
-	tar = tarfile.open(local_package_path)
+	try:
+		tar = tarfile.open(local_package_path)
+	except Exception:
+		import pdb;pdb.set_trace()
 	manifest = tar.extractfile(MANIFEST_FILENAME)
 
 	manifest_obj = json.loads(manifest.read())
@@ -83,8 +116,9 @@ def extract_dependencies(local_package_path):
 
 
 def download_package(pkg, recursive=True):
-	print("Downloading {package_name}".format(package_name=pkg["name"]))
+	print("Started downloading {package_name}".format(package_name=pkg["name"]))
 	local_package_path = download_single_package(pkg["name"], pkg.get("version"))
+	print("Finished downloading {package_name}".format(package_name=pkg["name"]))
 
 	if recursive:
 		dependency_pkg_names = extract_dependencies(local_package_path)
